8a1f0640c65999a4d5337007306584e9e234a8ee
BobRay <BobRay@24346d65-fb89-4855-a768-f5fb827ddd76>
2008-09-28 07:11:26 +0000

    First working version 3.0.5; Modified build.transport.php
    git-svn-id: http://svn.modxcms.com/svn/modx-components/ezfaq/trunk@176 24346d65-fb89-4855-a768-f5fb827ddd76

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -4,6 +4,27 @@ $mtime = explode(" ", $mtime);
 $mtime = $mtime[1] + $mtime[0];
 $tstart = $mtime;
 
+$sources= array (
+    'root' => dirname(dirname(__FILE__)) . '/',
+    'assets' => dirname(dirname(__FILE__)) . '/assets/',
+);
+
+$element_namespace = 'ezfaq';
+$element_name = 'EZfaq';
+$element_object_type = 'modSnippet';
+$element_type = 'snippet';
+$element_description = '<strong>3.0.5-beta</strong> Generates a FAQ page for your site.';
+$element_source_file = $sources['assets'] . 'ezfaq/snippet.ezfaq.php';
+$element_filename = 'snippet.ezfaq.php';
+$element_category = 0;
+$package_name = $element_name;
+$package_version = '3.0.5';
+$package_release = 'beta';
+$lexicon_path = $sources['root'] . '_build/lexicon/';
+$resolver_source = $sources['assets'] . 'ezfaq';
+$resolver_target = "return MODX_ASSETS_PATH . 'snippets/';";
+
+
 // get rid of time limit
 set_time_limit(0);
 
@@ -17,21 +38,23 @@ $modx->initialize('mgr');
 
 $modx->loadClass('transport.modPackageBuilder','',false, true);
 $builder = new modPackageBuilder($modx);
-$builder->create('EZFaq','1.0.5','beta');
-$builder->registerNamespace('ezfaq',false,true);
+$builder->create($package_name,$package_version,$package_release);
+$builder->registerNamespace($element_namespace,false,true);
 
-$sources= array (
-    'root' => dirname(dirname(__FILE__)) . '/',
-    'assets' => dirname(dirname(__FILE__)) . '/assets/',
-);
+if (!file_exists($element_source_file)) {
+    echo "<b>Error</b> - Element source file not found: {$element_source_file}<br>";
+    exit();
+}
 
-// get the source from the actual snippet in your database OR
+print "Creating element from source file: {$element_source_file}<br>";
+
+// get the source from the actual element in your database OR
 // manually create the object, grabbing the source from a file
-$c= $modx->newObject('modSnippet');
-$c->set('name', 'EZFaq');
-$c->set('description', '<strong>1.0.5-beta</strong> Generates a FAQ page for your site.');
-$c->set('category', 0);
-$c->set('snippet', file_get_contents($sources['assets'] . 'ezfaq/snippet.ezfaq.php'));
+$c= $modx->newObject($element_object_type);
+$c->set('name', $element_name);
+$c->set('description', $element_description);
+$c->set('category', $element_category);
+$c->set($element_type, file_get_contents($element_source_file));
 
 // create a transport vehicle for the data object
 $attributes= array(
@@ -39,14 +62,30 @@ $attributes= array(
     XPDO_TRANSPORT_UPDATE_OBJECT => true,
 );
 $vehicle = $builder->createVehicle($c, $attributes);
+
+echo "<br>Creating Resolver<br>";
+
+if (!is_dir($resolver_source)) {
+    echo "<b>Error</b> - Resolver source directory not found: {$resolver_source}<br>";
+    exit();
+}
+echo "&nbsp;&nbsp;&nbsp;&nbsp;Source: {$resolver_source}<br>";
+echo "&nbsp;&nbsp;&nbsp;&nbsp;Target: {$resolver_target}<br><br>";
+
 $vehicle->resolve('file',array(
-    'source' => $sources['assets'] . 'ezfaq',
-    'target' => "return MODX_ASSETS_PATH . '/';",
+    'source' => $resolver_source,
+    'target' => $resolver_target,
 ));
 $builder->putVehicle($vehicle);
 
+if (!is_dir($lexicon_path)) {
+    echo "<b>Error</b> - Lexicon path not found: {$lexicon_path}";
+    exit();
+}
+echo "Creating lexicon from: {$lexicon_path}<br><br>";
+
 // load lexicon strings
-$builder->buildLexicon($sources['root'].'_build/lexicon/');
+$builder->buildLexicon($lexicon_path);
 
 // zip up the package
 $builder->pack();
@@ -58,6 +97,6 @@ $tend= $mtime;
 $totalTime= ($tend - $tstart);
 $totalTime= sprintf("%2.4f s", $totalTime);
 
-echo "\nExecution time: {$totalTime}\n";
+echo "Package completed<br>Execution time: {$totalTime}<br>";
 
 exit ();
---
+++ assets/ezfaq/snippet.ezfaq.php
@@ -0,0 +1,294 @@
+<?php
+/**
+ * @package captcha
+ * @author Bob Ray <bobray@softville.com>
+ * @created 09-27-2008
+ * @version 3.0.5
+*/
+/*
+(based on an idea from SorenG and JavaScript code from DynamicDrive.com)
+Compatibility: MODX Revolution
+Thanks to: jaredc for suggestions on .css usage and cssPath parameter
+ */
+
+/******************************
+*             Usage           *
+*******************************
+
+
+Minimal EZfaq call:
+-------------------
+
+[[!EZfaq? &docID=`12`]]
+
+(Use the document ID of your unpublished FAQ content document
+-- NOT THE DOCUMENT THAT *DISPLAYS* THE FAQ --.)
+
+&docID is required.
+
+Optional parameters:
+--------------------
+
+&showHideAllOption [display the show/hide all buttons - default "true"]
+
+&statusOpenHTML  [symbol to put next to open topics (can be an image URL) - defaults to "[-]"]
+
+&statusClosedHTML [symbol to put next to closed topics (can be an image URL) - defaults to "[+]"]
+
+&openColor  [color for open questions (name or hex value #ffffff) - default "red"]
+
+&closedColor [color for closed questions (name or hex value #ffffff) - default "black"]
+
+&setPersist  [does open state persist across visits/reloads - default "true";]
+
+&collapsePrevious [if set, only one answer can be open at a time - default "true"]
+
+&defaultExpanded  [expand answers n1 through n2 ("0,1" expands items 1 through 2)when page is opened (default, no)]
+
+&cssPath [Optional Path to .css file -- set to `` for no .css file]
+
+Create a published document to display the FAQ. Put the snippet call in that document.
+Then create an unpublished document to hold the FAQ content (it makes sense for it to be a child of the first doc).
+Make a note of the document ID of the FAQ content document to use in the snippet call.
+
+File Format - The unpublished FAQ content document should be in the following format:
+-------------------------------------------------------------------------------------
+This will be rendered above the first question (optional)
+
+Q:How much is two plus two?
+
+A:Four
+
+Extra: This will always be rendered between questions. (optional)
+
+Q:Who is the fifth avatar of Vishnu?
+
+A:Vamana the Dwarf.
+
+Q:What is Sigmund Freud's middle name?
+
+A:Schlomo
+
+(etc.)
+Q:END-FAQ
+
+Work in progress can go here and won't be displayed. (optional)
+
+
+
+Available open/closed image pairs
+---------------------------------
+
+Open            Closed
+----------------------
+minus.png        plus.png
+minus2.png       plus2.png  (these have outlines)
+check.png        x.png
+
+Image URL full example:
+
+[[EZfaq? &docID=`12` &statusOpenHTML=`<img srcEQUALS"assets/snippets/ezfaq/images/minus.png">` &statusClosedHTML=`<img srcEQUALS"assets/snippets/ezfaq/images/plus.png">`]]
+
+
+Styling EZfaq
+-------------
+Some styling is accomplished with the parameters in the snippet call. Other styling issues require changes to the file:
+/assets/snippets/ezfaq/ezfaq.css
+
+
+
+Using Flash Videos and lightbox in the FAQ
+An FAQ answer can be in the form of a Flash video using the MODx swfObject snippet (http://modxcms.com/SWFObject-1815.html) and the following format:
+
+Q:How do I play Flash in here?
+
+A:[[swfObject? &swfid=`0` &swfFile=`assets/flash/playback.swf` &swfWidth=`325` &swfHeight=`155`]]
+
+For lightbox images, use:
+
+Q:How do I work with lightbox?
+
+A:<a href="http://domain.com/assets/images/small-pic.jpg" rel="lightbox"><img src="http://domain.com/assets/images/large-pic.jpg" alt="" width="120" height="120" border="0" /></a>
+
+***************************************************
+*             Code begins here                    *
+***************************************************/
+
+
+/* set path to resources */
+
+$faqPath = MODX_BASE_URL . "assets/snippets/ezfaq/";
+
+$error_message = "";
+
+/* get the lexicon entries for the EZfaq prompts */
+
+$modx->lexicon->load('ezfaq:default');
+
+
+/* ********************************************
+Make sure we have what we need before proceeding
+***********************************************/
+
+if (!isset($docID)) {  // user didn't send docID parameter
+    return $modx->lexicon('ezfaq-docID-required');
+}
+
+$doc = $modx->getDocument((string)$docID, '*', 1); // Search published first.
+if (empty($doc)) {
+    $doc = $modx->getDocument((string)$docID, '*', 0); // Un-published next?
+}
+if (empty($doc)) { // user requested a non-existing document
+   return $modx->lexicon('ezfaq-doc-not-found');
+}
+
+/****************************************************************************************
+Now that we have the language file and the document with the FAQ
+content, we'll plug in the .css and .js and  initialize the optional parameters.
+*****************************************************************************************/
+
+/*  Inject .css file into document header - maybe.
+    Use $cssPath=`` if you want to put the .css in your site's .css file
+    rather than using a separate .css file for the FAQ  */
+
+if (isset($cssPath) ) {    // user has set this parameter
+    if ($cssPath == "") {
+        // do nothing, user doesn't want a separate .css file
+    } else { // user has specified the .css file to use
+        $src = $cssPath;
+        $modx->regClientCSS($src);
+    }
+} else { // not set, use the default .css file
+
+    $src = $faqPath."ezfaq.css";
+    $modx->regClientCSS($src);
+}
+
+/* inject the .js into the document header. */
+
+$src = $faqPath."switchcontent.js";
+$modx->regClientStartupScript($src);
+
+
+/*Show the buttons that show and hide all answers. */
+
+$showHideAllOption = isset($showHideAllOption)?$showHideAllOption:true;
+
+/* Set prefix markers for open and closed answers (can be string or an image URL). Defaults to plus and minus signs. */
+$statusOpenHTML = isset($statusOpenHTML)?$statusOpenHTML:"[-]";
+$statusClosedHTML = isset($statusClosedHTML)?$statusClosedHTML:"[+]";
+
+/* Set colors for open and closed (applies to question; answer is styled in .css). Can be color name or hex color value (#ffffff); */
+$openColor = isset($openColor)?$openColor:"red";
+$closedColor = isset($closedColor)?$closedColor:"black";
+
+/* set whether state persists on return visits  */
+$setPersist = isset($setPersist)?$setPersist:"true";
+
+/* when set to true, only one answer can be expanded at a time (default) */
+$collapsePrevious = isset($collapsePrevious)?$collapsePrevious:"true";
+
+/* expand answers n1 through n2 when page is opened (default, no)to set, use "0,2" to expand first three answers, "0" to expand just the first */
+$defaultExpanded = isset($defaultExpanded)?$defaultExpanded:"";
+
+/************************************
+ *  Work starts here
+*************************************/
+
+/* replace "EQUALS" in URLs with "=" */
+
+$statusOpenHTML = str_replace("EQUALS","=",$statusOpenHTML);
+$statusClosedHTML = str_replace("EQUALS","=",$statusClosedHTML);
+
+
+$output = "";
+
+$docString = $doc['content'];
+
+
+
+if ($showHideAllOption) {
+
+$output .= '<div class="faqExpand">';
+$output .= '<p>'.$modx->lexicon('ezfaq-show-hide-msg').'</p>';
+
+$output .= '<a href="javascript:faq.sweepToggle('."'expand')".'"> '.$modx->lexicon('ezfaq-expand-button-msg').' </a>';
+$output .='&nbsp;&nbsp;&nbsp;&nbsp;';
+$output .= '<a href="javascript:faq.sweepToggle('."'contract')".'"> '.$modx->lexicon('ezfaq-contract-button-msg').' </a>';
+
+$output .= '</div>';
+
+}
+
+
+
+$docArray = explode("Q:",$docString);
+
+/* for debugging  */
+
+//$i = count($docArray);
+// echo "count: ".$i.'<br>';
+
+$itemCount=0; // used to make every id different
+
+
+$output .= '<div class="faqContainer">'."\n";
+foreach($docArray as $value) {
+
+
+    if ($itemCount==0) {  // very first item (or pre first item)
+        if (!strstr($value,"A:")) {   // no answer, assume it's before the beginning
+           $output .= $value;  // send it out without processing
+           continue;  // got to the next (first real item)
+        }
+    } else {
+        //normal item, continue
+    }
+  $items = explode("A:",$value);
+      if (stristr($items[0],"FAQ-END")) {
+         break;
+      }
+
+  if ( ($items[0] != "") && ($items[1] != "") ) {
+
+     $output .= '<p id="faq'.$itemCount.'-title" class="handcursor">';
+     $output .= '<span class="faqQuestion">';
+     $output .= $items[0];
+     $output .= '</span>';
+     $output .= '</p>';
+     $aArray = explode("Extra:",$items[1]);
+     $output .= '<div id="faq'.$itemCount.'" class="switchgroup1">';
+     $output .= $aArray[0];
+     $output .= '</div>';
+
+     if ($aArray[1] != "") {
+       $output .= $aArray[1];
+     }
+
+  }
+  $itemCount++;
+}
+$output .= '</div>'."\n"; // end of faqContainer div
+
+//   MAIN FUNCTION: new switchcontent("class name", "[optional_element_type_to_scan_for]") REQUIRED
+//1) Instance.setStatus(openHTML, closedHTML)- Sets optional HTML to prefix the headers to indicate open/closed states
+//2) Instance.setColor(openheaderColor, closedheaderColor)- Sets optional color of header when it's open/closed
+//3) Instance.setPersist(true/false)- Enables or disabled session only persistence (recall contents' expand/contract states)
+//4) Instance.collapsePrevious(true/false)- Sets whether previous content should be contracted when current one is expanded
+//5) Instance.defaultExpanded(indices)- Sets contents that should be expanded by default (ie: 0, 1). Persistence feature overrides this setting!
+//6) Instance.init() REQUIRED
+
+$output .= '<script type="text/javascript">;'."\n";
+$output .= 'var faq=new switchcontent("switchgroup1", "div");'."\n";  //Limit scanning of switch contents to just "div" elements
+
+
+$output .= "faq.setStatus('".$statusOpenHTML." ','".$statusClosedHTML." ');"."\n"; // set open/closed indicators
+$output .= 'faq.setColor("'.$openColor.'", "'.$closedColor.'");'."\n"; // set open/closed text colors
+$output .= 'faq.setPersist('.$setPersist.');'."\n";
+$output .= 'faq.collapsePrevious('.$collapsePrevious.');'."\n"; //Only one content open at any given time
+$output .= 'faq.defaultExpanded("'.$defaultExpanded.'");'."\n"; // expand some on open?
+$output .= 'faq.init();'."\n";
+$output .= '</script>'."\n";
+
+return $output;
+?>
