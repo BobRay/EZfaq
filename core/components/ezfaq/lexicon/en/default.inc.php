d54f47c06c0b1e01e72fbaffc5d8e8098397d67d
bobray <bobray@softville.com>
2012-06-23 15:54:58 -0500

    corrected error message for doc ID property

--- core/components/ezfaq/lexicon/en/default.inc.php
+++ core/components/ezfaq/lexicon/en/default.inc.php
@@ -5,7 +5,7 @@
  * @package ezfaq
  */
 
-$_lang['ezfaq-docID-required'] = "docID is a required parameter\n";
+$_lang['ezfaq-docID-required'] = "ezfaqDocID is a required parameter\n";
 $_lang['ezfaq-doc-not-found'] = "Can't find a document with that ID\n";
 $_lang['ezfaq-show-hide-msg'] = "Click on a question to expand the answer";
 $_lang['ezfaq-expand-button-msg'] = "Show All";
